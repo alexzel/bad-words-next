de672e323ca4e0dd077ee4e26c1873bfe8f8a945
Alex Zelensky <alexzgit@gmail.com>
2023-01-30 22:14:53 -0800

    Add more benchmark runs with different input

--- benchmark/README.md
+++ benchmark/README.md
@@ -14,11 +14,65 @@ node ./benchmark.js
 
 ## Results on MacBook Pro (16-inch, 2021)
 
+### Run #1
+
+#### Benchmark results
+
 |Task Name|Average Time (ps)|Variance (ps)|
 |---------|-----------------|-------------|
-|**BadWordsNext:check**|**0.06812153359276872**|**0.017196433033053415**|
-|LeoProfanity:check|5.292628132561964|0.006905092296305512|
-|BadWords:check|341.4517864839209|547.9307879398782|
-|**BadWordsNext:filter**|**3.393507880793036**|**0.03768847281324516**|
-|LeoProfanity:filter|12.32597279251834|0.04510877200234826|
-|BadWords:filter|7082.269469896953|534.5598664437144|
+|**BadWordsNext:check**|**0.06638201297733286**|**0.004539486163132269**|
+|LeoProfanity:check|7.222238607662132|0.020526526087941182|
+|BadWords:check|369.0270471396922|652.80899558365|
+|**BadWordsNext:filter**|**6.369680796459222**|**0.07329345018353091**|
+|LeoProfanity:filter|16.473531133946743|0.07108016436329749|
+|BadWords:filter|13935.916721820831|1550.203777424369|
+
+#### Check results
+
+```js
+BadWordsNext:check true
+LeoProfanity:check true
+BadWords:check true
+```
+
+### Run #2
+
+#### Benchmark results
+
+|Task Name|Average Time (ps)|Variance (ps)|
+|---------|-----------------|-------------|
+|**BadWordsNext:check**|**0.06945916290377001**|**0.004652467607761704**|
+|LeoProfanity:check|5.216321750684845|0.00838680288150679|
+|BadWords:check|271.1025447380252|1.792571563179542|
+|**BadWordsNext:filter**|**3.208444776965852**|**0.024105265604765492**|
+|LeoProfanity:filter|12.008501547786848|0.028839924213570994|
+|BadWords:filter|6917.233117421469|501.0235655967264|
+
+#### Check results
+
+```js
+BadWordsNext:check true
+LeoProfanity:check false // this check result is wrong
+BadWords:check true
+```
+
+### Run #3
+
+#### Benchmark results
+
+|Task Name|Average Time (ps)|Variance (ps)|
+|---------|-----------------|-------------|
+|**BadWordsNext:check**|**0.0684184871491811**|**0.0005664190186490255**|
+|LeoProfanity:check|145.54107934236526|0.41215264071434377|
+|BadWords:check|1716.5473154035665|227.85273467455897|
+|**BadWordsNext:filter**|**0.07221527299207924**|**0.0004734945680949354**|
+|LeoProfanity:filter|329.1995580259122|1.6238536398754997|
+|BadWords:filter|245295.5917954445|41091.74011662451|
+
+#### Check results
+
+```js
+BadWordsNext:check false
+LeoProfanity:check false
+BadWords:check false
+```
--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -8,39 +8,67 @@ const leo = require('leo-profanity')
 const BadWordsNext = require('../')
 const en = require('../data/en.json')
 
-const bench = new Bench({ time: 100 })
-
-const badwordsNext = new BadWordsNext({ data: en })
 const badwords = new BadWords()
+const badwordsNext = new BadWordsNext({ data: en })
 
-const BAD = 'S0me sh!tt is here. just\t $hittt \n   and \ng0 \n@$hol b00bs and d1cks \n end'
+let counter = 0
+const run = str => {
+  const bench = new Bench({ time: 100 })
 
-bench
-  .add('BadWordsNext:check', () => {
-    badwordsNext.check(BAD)
-  })
-  .add('LeoProfanity:check', () => {
-    leo.check(BAD)
-  })
-  .add('BadWords:check', () => {
-    badwords.isProfane(BAD)
-  })
-  
-  .add('BadWordsNext:filter', () => {
-    badwordsNext.filter(BAD)
-  })
-  .add('LeoProfanity:filter', () => {
-    leo.clean(BAD)
-  })
-  .add('BadWords:filter', () => {
-    badwords.clean(BAD)
-  })
-  
-  .run()
-  .then(() => {
-    console.table(bench.tasks.map(({ name, result = {} }) =>
-      ({ "Task Name": name, "Average Time (ps)": (result.mean || 0) * 1000, "Variance (ps)": (result.variance || 0) * 1000 })));
-  })
-  .catch(e => {
-    console.error(e)
-  })
+  bench
+    .add('BadWordsNext:check', () => {
+      badwordsNext.check(str)
+    })
+    .add('LeoProfanity:check', () => {
+      leo.check(str)
+    })
+    .add('BadWords:check', () => {
+      badwords.isProfane(str)
+    })
+
+    .add('BadWordsNext:filter', () => {
+      badwordsNext.filter(str)
+    })
+    .add('LeoProfanity:filter', () => {
+      leo.clean(str)
+    })
+    .add('BadWords:filter', () => {
+      badwords.clean(str)
+    })
+
+    .run()
+    .then(() => {
+      console.log('\x1b[34m%s\x1b[0m', `Run #${++counter}`)
+      console.log()
+      console.log('\x1b[32m%s\x1b[0m', 'Input string:')
+      console.log()
+      console.log(str)
+      console.log()
+      console.log('\x1b[32m%s\x1b[0m', 'Benchmark results:')
+      console.log()
+      console.table(bench.tasks.map(({ name, result = {} }) =>
+        ({ 'Task Name': name, 'Average Time (ps)': (result.mean || 0) * 1000, 'Variance (ps)': (result.variance || 0) * 1000 })));
+      console.log()
+      console.log('\x1b[32m%s\x1b[0m', 'Check results:')
+      console.log()
+      console.log('BadWordsNext:check', badwordsNext.check(str))
+      console.log('LeoProfanity:check', leo.check(str))
+      console.log('BadWords:check', badwords.isProfane(str))
+      console.log()
+    })
+    .catch(e => {
+      console.error(e)
+    })
+}
+
+run('Test word sex here and many words in this string as well as many possibilities to check and filter. We test this string and get some results.')
+run('S0me sh!tt is here. just\t $hittt \n   and \ng0 \n@$hol b00bs and d1cks \n end')
+run(`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vel dolor at felis tempor euismod. Integer ac dictum lorem, quis malesuada enim. Ut at lacinia nibh, vitae faucibus lectus. Morbi efficitur sodales eros et hendrerit. Praesent porttitor fringilla nunc, nec malesuada nisl faucibus sit amet. Cras leo est, finibus quis ligula sed, lacinia feugiat tortor. Suspendisse pulvinar dui massa, id pulvinar sapien porta vitae.
+
+Morbi mollis sapien at justo hendrerit, non ullamcorper dui vehicula. Praesent venenatis tellus a ante laoreet egestas sit amet non nibh. Suspendisse commodo malesuada leo sit amet pharetra. Aliquam sed interdum est, id hendrerit ante. Vestibulum quis leo id leo viverra ultricies. Quisque eu accumsan nisl, cursus mollis ipsum. Sed tincidunt eleifend sem, eu dignissim quam accumsan vitae. Ut cursus enim eu porta maximus. Nullam eget purus convallis, commodo mauris nec, dapibus ante. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+
+Praesent at interdum orci. Vestibulum ligula velit, imperdiet non convallis in, placerat sit amet lorem. Nullam posuere enim et lectus vulputate, vel rhoncus ipsum posuere. In nec viverra turpis. Ut tempus nibh ac bibendum dapibus. Aenean efficitur, dui rutrum vestibulum suscipit, magna velit interdum odio, vitae tempor lectus nisi eu ex. In bibendum porttitor placerat. Vestibulum vestibulum vulputate justo eget congue. Pellentesque ut augue et augue fermentum hendrerit vel et ante. Donec ex mi, mattis vitae turpis at, posuere aliquam erat. Proin tincidunt elit et velit venenatis accumsan.
+
+Fusce eu venenatis lorem. Phasellus ligula lorem, scelerisque in interdum nec, aliquam vitae orci. Duis in ullamcorper erat, non placerat justo. Nunc fringilla metus felis, eget volutpat urna sodales vitae. In semper bibendum lobortis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas in leo sit amet turpis venenatis venenatis quis sed sapien. Fusce quis sollicitudin purus. Etiam ac mi ultrices sapien efficitur condimentum in dictum ante. Praesent eros sapien, ornare eget tellus at, malesuada varius massa. Nunc egestas ex est. Vestibulum eget lorem libero. Aliquam nec est eu nunc porttitor iaculis. Morbi erat ex, tristique a vestibulum nec, convallis ac nulla. Quisque vel quam iaculis, fringilla purus vel, euismod dui. Phasellus efficitur, odio non feugiat fermentum, purus dolor scelerisque eros, et tincidunt magna ante at sapien.
+
+In venenatis fringilla nulla, quis dignissim dolor tempus tempus. Nunc vel justo convallis, fringilla libero sit amet, molestie magna. In at condimentum dolor, quis viverra augue. Praesent scelerisque, justo vel dictum pretium, urna massa porttitor nulla, vel condimentum nisi nisl vel augue. Quisque in nibh in lectus condimentum ultricies id eu velit. Phasellus tempor scelerisque libero rutrum placerat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur luctus sodales justo sit amet vulputate. Curabitur venenatis arcu risus, non commodo orci viverra non. Praesent congue, diam ac aliquam vehicula, libero ligula posuere orci, vel feugiat leo nulla nec purus. Curabitur efficitur nunc erat, in elementum purus elementum eget.`)
